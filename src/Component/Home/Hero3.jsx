29b6e2134faacbf127124e2e58bdac61d48bdf05
maxamedshucayb986-collab <maxamedshucayb986@gmail.com>
2025-10-25 19:14:40 +0300

    update

--- src/Component/Home/Hero3.jsx
+++ src/Component/Home/Hero3.jsx
@@ -9,7 +9,7 @@ export default function Section2() {
       <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-10 items-center">
         {/* Sawirka Dugsiga */}
         <motion.img
-          src="https://scontent.fmgq1-2.fna.fbcdn.net/v/t39.30808-6/487324465_1246388560617307_299250803992270810_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=UViTkhtlldoQ7kNvwGn28MJ&_nc_oc=AdmwN3soiYBzx5p9R7bwrdwO21P0olcZwA8-EzCd4wsbgoi-yXHr7-V3wYrpVUX2nx8&_nc_zt=23&_nc_ht=scontent.fmgq1-2.fna&_nc_gid=iHPi-8yUIBYbuH2YHuE-VA&oh=00_AfcC_NYm2WHKYvk7U4uCY9dsXoTmj94nTnmw09xVBwvlyw&oe=68F9770D"
+          src="https://scontent.fmgq1-2.fna.fbcdn.net/v/t39.30808-6/487324465_1246388560617307_299250803992270810_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Z0699Rp5zNgQ7kNvwHBsBtn&_nc_oc=Adl-Q3kO9T1NTwd7OmpY9Gz6dUP8rJrFnFWqbVFdaZZ4yGEvZr8I56Np7vwGbbKqiIw&_nc_zt=23&_nc_ht=scontent.fmgq1-2.fna&_nc_gid=YD1jqvAZuDKyiDSADkCMXQ&oh=00_AfcJmY8-99pINgut3GYkJjhYSM2mW3OlfZzLDeYhUa5tTA&oe=6902B18D"
           alt="School Building"
           className="rounded-2xl shadow-lg"
           initial={{ opacity: 0, x: -50 }}
